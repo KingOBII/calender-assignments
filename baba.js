76ba2559d8ebb8a4ecb61b03d46f714ba2481fc9
KingOBII <114228560+KingOBII@users.noreply.github.com>
2022-12-04 18:44:58 +0100

    Add files via upload

---
+++ baba.js
@@ -0,0 +1,10 @@
+var fact1 =  5;
+
+for(var i= fact1-1;i>=1 ;i--){
+	fact1=fact1*i 
+}
+console.log(fact1); 
+
+var tt="<p> I am a busy</p>"
+var ss="<p> I am a good boy>"
+ console.log(ss);
---
+++ blaise.js
@@ -0,0 +1,11 @@
+var f1=0;
+var f2=1;
+var i=0;
+var plus
+
+for(i=0; i<10 ;i++){
+	console.log(f1);
+  sum=f1+f2;
+   f1=f2;
+   f2=number(plus);
+}
